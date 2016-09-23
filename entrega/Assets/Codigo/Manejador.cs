944c4b68114c9d13430f3bb8ee0409a608e64013
PATRINOT\Patricia <Nahuelwenzin@outlook.com>
2016-09-23 16:31:05 -0300

    Todo terminado dice patrinot pq esta era una notebook de mi vieja y la estoy usando yo. No no soy un travesti llamado Patricia  los fines de semana

---
+++ entrega/Assets/Codigo/Manejador.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manejador : MonoBehaviour
+{
+    Animator Animaciones;
+    // Use this for initialization
+    void Start()
+    {
+        Animaciones = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Animaciones.SetInteger("Stado", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            Animaciones.SetInteger("Stado", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Animaciones.SetInteger("Stado", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            Animaciones.SetInteger("Stado", 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Animaciones.SetInteger("Stado", 3);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            Animaciones.SetInteger("Stado", 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Animaciones.SetInteger("Stado", 4);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            Animaciones.SetInteger("Stado", 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Animaciones.SetInteger("Stado", 5);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha5))
+        {
+            Animaciones.SetInteger("Stado", 0);
+
+        }
+    }
+}
+
